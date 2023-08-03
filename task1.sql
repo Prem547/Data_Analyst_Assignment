2cb478a32ac5ba8e550ef31730b55f307f89f50c
Prem <53786841+Prem547@users.noreply.github.com>
2023-08-03 22:16:01 +0530

    task1.sql

--- task1.sql
+++ task1.sql
@@ -30,3 +30,16 @@ duckdb> WITH events(id, occur_time, close_time) AS (
    ...> FROM tasks_with_task_id
    ...> GROUP BY id, task_id
    ...> ORDER BY id, task_id;
+
+--Explanation:
+
+--The code starts with a Common Table Expression (CTE) named events. The CTE events defines the temporary table that holds the event data with columns id, occur_time, and close_time. The VALUES clause is used to provide the sample data for these columns.
+
+--The next CTE named tasks_with_task_id is defined. In this CTE, we calculate the task_id for each event occurring before its corresponding close_time. The subquery within this CTE uses a correlated subquery to find the number of distinct occur_time values from the events table (aliased as e2) where the id matches the current row's id and the occur_time is less than the current row's close_time. This count essentially gives us the task_id for each event based on the chronological order of the events.
+
+--The final SELECT statement retrieves the desired output columns: id, task_id, MIN(occur_time) (earliest occur_time within each task), and MAX(close_time) (latest close_time within each task). The results are grouped by id and task_id, and the rows are ordered by id and task_id.
+
+--The final output displays the task_id, earliest occur_time, and latest close_time for each task, as required in the task description.
+
+--The query efficiently assigns task_ids to events occurring before their corresponding close_times and calculates the required metrics for each task. The results are sorted based on id and task_id, which helps in visualizing the tasks in chronological order.
+
